253eb04b66745a87f2677d58c8178d88859d1231
Kamran <cshahraray@gmail.com>
2021-08-10 23:16:13 -0400

    Create basic color picker using react-color library.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChromePicker } from 'react-color'
+import React, { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('#fff');
+  const [showColorPicker, setShowColorPicker] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button onClick={ () => setShowColorPicker(!showColorPicker)} > Toggle </button>
+    { showColorPicker && ( 
+      <ChromePicker
+      color={color}
+      onChange={ updatedColor => setColor(updatedColor)}
+      />
+    )} 
     </div>
   );
 }
