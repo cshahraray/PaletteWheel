d55c458c4b2cbb3dc8713c0f773c7ff6ce188f0b
Kamran <cshahraray@gmail.com>
2021-08-11 15:00:00 -0400

    Color wheel calculates RGB value based on arc angle of mouse click.

--- src/components/color-circle.js
+++ src/components/color-circle.js
@@ -1,44 +1,83 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { rad2Deg } from '../utils/circle-math-utils'
+import { hsv2rgb } from '../utils/colorspace-utils'
 
 const ColorCircle = props => {
-
+  //state variables
   const canvasRef = useRef(null)
   const [mouseX, setMouseX] = useState(0)
   const [mouseY, setMouseY] = useState(0)
   const [centerXY] = useState([95, 45])
+  const [radius] = useState(40)
   const [deltaX, setDeltaX] = useState(0)
   const [deltaY, setDeltaY] = useState(0)
+  const [angle, setAngle] = useState(0)
+  const [wheelColor, setWheelColor] = useState('rgb(255,50,50)')
   
-  const drawCircle = (ctx) => {
-      ctx.fillStyle = 'red'
-      ctx.beginPath();
-      ctx.arc(centerXY[0], centerXY[1], 40, 0, 2 * Math.PI);
-      ctx.stroke();      
-      ctx.fill()
-
+  //helper methods
+  const getAngle = () => {
+    return rad2Deg(Math.atan2(deltaY, deltaX))
   }
 
+  const angle2Color = () => {
+    let rgbArr = hsv2rgb(angle, .5, .5)
+    console.log(angle)
+    console.log(rgbArr)
+    return `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`
+  }
+
+  //canvas methods
+
+  const resizeCanvasToDisplaySize = (canvas) => {
+    
+    const { width, height } = canvas.getBoundingClientRect()
+
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width
+      canvas.height = height
+      return true // here you can return some usefull information like delta width and delta height instead of just true
+      // this information can be used in the next redraw...
+    }
+    return false
+  }
+
+  const drawCircle = (ctx) => {
+      ctx.beginPath();
+      ctx.arc(centerXY[0], centerXY[1], radius, 0, 2 * Math.PI);
+      ctx.stroke();      
+      ctx.fill()
+  }
+  
+  //event methods
+  const onClick = (e) => {
+      setMouseX(e.offsetX)
+      setMouseY(e.offsetY)
+      setDeltaX(mouseX - centerXY[0])
+      setDeltaY(mouseY - centerXY[1])
+      setAngle(getAngle())
+      setWheelColor(angle2Color())
+  }
+  
+  //react methods
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
     canvas.addEventListener('click', onClick, false)
-    //Our first draw  
-
+    //Our first draw
+    context.fillStyle = `${wheelColor}`
     drawCircle(context)
   }, )
-  
-  const onClick = (e) => {
-      setMouseX(e.offsetX)
-      setMouseY(e.offsetY)
-  }
-  
   return (
     <>
     <canvas ref={canvasRef} {...props}/>
+    {canvasRef.current && resizeCanvasToDisplaySize(canvasRef.current)}
     {mouseX}
     {mouseY}
     <br></br>
     {centerXY}
+    <br></br>
+    {getAngle()}
+    {wheelColor}
     </>
   )
 }
---
+++ src/utils/circle-math-utils.js
@@ -0,0 +1,3 @@
+export const rad2Deg = (radian) => {
+    return (radian + Math.PI) / (2 * Math.PI) * 360;
+}
